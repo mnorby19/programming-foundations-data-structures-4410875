94e0a92d90435e475533d3480a88d95f5c4fcb98
mnorby19 <129117659+mnorby19@users.noreply.github.com>
2024-09-12 15:26:13 +0000

    y

--- 02/02_05/02_05e/main.py
+++ 02/02_05/02_05e/main.py
@@ -8,5 +8,9 @@ seating_chart = [
 # print(seating_chart[2][1])
 
 for i, row in enumerate(seating_chart):
+    print(f"In row {i+1} is {row}")
+
+
+for i, row in enumerate(seating_chart):
     for j, student_name in enumerate(row):
         print(f"{student_name} is in row {i+1}, seat {j+1}")
