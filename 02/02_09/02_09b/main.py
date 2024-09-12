c128202fa04d0ebafbc7fab71a2fb129e3e0fb26
mnorby19 <129117659+mnorby19@users.noreply.github.com>
2024-09-12 16:24:42 +0000

    y

--- 02/02_09/02_09b/main.py
+++ 02/02_09/02_09b/main.py
@@ -1,4 +1,9 @@
 def find_second_smallest(my_list):
-    return 0
+    if len(my_list) <= 1:
+        return None
+    sorted_list = sorted(my_list, reverse = False)
+    return sorted_list[1]
 
 print(find_second_smallest([5, 8, 3, 2, 6]))
+print(find_second_smallest([]))
+print(find_second_smallest([2]))
--- 02/02_09/02_09e/main.py
+++ 02/02_09/02_09e/main.py
@@ -1,4 +1,20 @@
 def find_second_smallest(my_list):
     return 0
 
+
+def find_second_smallest_v2(my_list):
+    if len(my_list) <= 1:
+        return None
+    smallest = float('inf')
+    second_smallest = float('inf')
+ 
+    for item in my_list:
+        if item < smallest:
+            second_smallest=smallest
+            smallest=item
+        elif item < second_smallest:
+            second_smallest = item
+    return second_smallest
+
 print(find_second_smallest([5, 8, 3, 2, 6]))
+print(find_second_smallest_v2([5, 8, 3, 2, 6]))
